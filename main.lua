360cce0edf3bc882aa37a7b85e67c0e9db79c9dc
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-03-17 17:49:00 -0400

    First to commit

---
+++ main.lua
@@ -0,0 +1,7 @@
+print (" ")
+print ("AVS is the man!!!") 
+print ("This might be like Python lol")
+print ("Maybe LMAO")
+print ("We will be using Lua to make 2D games")
+print (" ")
+-- Make sure to add this to execute code at the terminal:    lua53 .\main.lua
